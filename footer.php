c08942593dc79b0ea2118e40530c6da5589a21f5
Irfan Durmus <irfandurmus@gmail.com>
2012-04-21 23:45:44 +0300

    analytics code removed

--- footer.php
+++ footer.php
@@ -3,19 +3,6 @@
 	Backend <a href="http://wordpress.com">WordPress</a> | Frontend <a href="http://github.com/irfan/homemade">Homemade</a>
 </footer>
         </div>
-        <script type="text/javascript">
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-1750484-4']);
-            _gaq.push(['_setDomainName', 'irfandurmus.com']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-
-        </script>
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
         <script src="<?php bloginfo('template_directory') ?>/prettify/prettify.js" type="text/javascript"></script>
         <script src="<?php bloginfo('template_directory') ?>/main.js" type="text/javascript"></script>
